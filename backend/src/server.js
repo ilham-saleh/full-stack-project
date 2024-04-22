6ff434f6582341f43a076b9bf75ab730b19d1255
ilham saleh <ilham2saleh@gmail.com>
2024-04-22 12:59:25 +0100

    Build for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,14 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import dotenv from "dotenv";
 dotenv.config();
 
 import { app, server } from "../socket/socket.js";
+
+const __dirname = path.resolve();
 
 //Tell express we want to use the morgan library
 app.use(morgan("dev"));
@@ -16,6 +19,13 @@ app.use(cors());
 //Tell express to parse JSON in the request body
 app.use(express.json());
 app.use(cookieParser());
+
+// Deployment
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 const port = process.env.PORT || 8000;
 
 import userRouter from "./routes/users.js";
